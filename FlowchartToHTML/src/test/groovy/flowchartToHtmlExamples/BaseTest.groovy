8421731e01011c0a9eed80851601a91505d8328c
Artur Boronat <arturboronat@gmail.com>
2023-09-09 16:49:31 +0100

    FIX for test assertions

--- FlowchartToHTML/src/test/groovy/flowchartToHtmlExamples/BaseTest.groovy
+++ FlowchartToHTML/src/test/groovy/flowchartToHtmlExamples/BaseTest.groovy
@@ -14,20 +14,23 @@ class BaseTest extends YAMTLModule {
     @Test
     def void testBase() {
         // model transformation execution example
-        def src_metamodel = YAMTLModule.loadMetamodel(BASE_PATH + '/flowchart.ecore') as EPackage
-        def tgt_metamodel = YAMTLModule.loadMetamodel(BASE_PATH + '/html.ecore') as EPackage
+        def srcRes = YAMTLModule.preloadMetamodel(BASE_PATH + '/flowchart.ecore') 
+        def tgtRes = YAMTLModule.preloadMetamodel(BASE_PATH + '/html.ecore') 
 
-        def xform = new Base(src_metamodel, tgt_metamodel)
+        def xform = new Base(srcRes.contents[0], tgtRes.contents[0])
 		YAMTLGroovyExtensions.init(this)
         xform.loadInputModels(['in': BASE_PATH + '/wakeup.xmi'])
         xform.execute()
         xform.saveOutputModels(['out': BASE_PATH + '/baseOutput.xmi'])
 		
 		// test assertion
-		def actualModel = xform.getOutputModel('out')
-		EMFComparator comparator = new EMFComparator();
-		xform.loadMetamodel(BASE_PATH + '/html.ecore', true)
+		def actualModel = xform.getOutputModel('out') 
+		EMFComparator comparator = new EMFComparator()
+		// Load the expected model using the identical output metamodel from the transformation.
+		// Essentially, use the same in-memory metamodel.
+		xform.loadMetamodelResource(tgtRes) 
 		def expectedResource = xform.loadModel(BASE_PATH + '/baseExpectedOutput.xmi', false)
-		assertTrue( comparator.equals(expectedResource.getContents(), actualModel.getContents()) );
+		def assertionResult =  comparator.equals(expectedResource.getContents(), actualModel.getContents()) 
+		assertTrue(assertionResult);
     }
 }
