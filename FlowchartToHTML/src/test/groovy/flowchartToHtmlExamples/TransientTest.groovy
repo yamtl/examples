abb4d863666e78d2bd0193e579e893661e2592b0
Artur Boronat <arturboronat@gmail.com>
2023-09-12 12:12:53 +0100

    ADD new Transient tests

---
+++ FlowchartToHTML/src/test/groovy/flowchartToHtmlExamples/TransientTest.groovy
@@ -0,0 +1,42 @@
+package flowchartToHtmlExamples
+
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.emf.common.util.URI
+import org.eclipse.emf.ecore.EPackage
+import org.eclipse.emf.ecore.resource.Resource
+import org.eclipse.emf.ecore.resource.ResourceSet
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl
+import org.junit.jupiter.api.Test
+
+import yamtl.core.YAMTLModule
+import yamtl.groovy.YAMTLGroovyExtensions
+import yamtl.utils.EMFComparator
+
+class TransientTest extends YAMTLModule {
+
+    final BASE_PATH = 'model'
+
+	@Test
+	def void testTransient() {
+		// model transformation execution
+		def srcRes = YAMTLModule.preloadMetamodel(BASE_PATH + '/flowchart.ecore')
+		def tgtRes = YAMTLModule.preloadMetamodel(BASE_PATH + '/html.ecore')
+
+		def xform = new Transient(srcRes.contents[0], tgtRes.contents[0])
+		YAMTLGroovyExtensions.init(this)
+		xform.loadInputModels(['in': BASE_PATH + '/wakeup.xmi'])
+		xform.execute()
+		xform.saveOutputModels(['out': BASE_PATH + '/transientOutput.xmi'])
+		
+//		// test assertion
+//		def actualModel = xform.getOutputModel('out')
+//		EMFComparator comparator = new EMFComparator();
+//		// Load the expected model using the identical output metamodel from the transformation.
+//		// Essentially, use the same in-memory metamodel.
+//		xform.loadMetamodelResource(tgtRes)
+//		def expectedResource = xform.loadModel(BASE_PATH + '/transientExpectedOutput.xmi', false)
+//		def assertionResult =  comparator.equals(expectedResource.getContents(), actualModel.getContents())
+	}
+}
