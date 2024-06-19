db9e0f0f486cd67f9e5c497a5e8e1efa0e9d22d3
arturboronat <artur.boronat@gmail.com>
2024-06-19 10:47:50 +0100

    Add examples QueryDsl

--- query_dsl/src/test/groovy/queryDsl/QueryActivityDsl_CD_test.groovy
+++ query_dsl/src/test/groovy/queryDsl/QueryActivityDsl_CD_test.groovy
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals
 class QueryActivityDsl_CD_test {
 
 	@Test
-	def void validate_class_unique_name() {
+	def void validate_class_unique_name_within_package() {
 		def mmPath = './model/CD.emf'
 		def modelPath = './model/cd_model.xmi'
 		
@@ -27,6 +27,28 @@ class QueryActivityDsl_CD_test {
 	}
 	
 	@Test
+	def void validate_class_unique_name_global() {
+		def mmPath = './model/CD.emf'
+		def modelPath = './model/cd_model.xmi'
+		
+		def fails = false;
+		def classNames = [] as Set<String>
+		
+		def queryDef = [
+			context: 'Class',
+			query: {
+				def added = classNames.add(it.name)
+				if (!added) fails=true
+			}
+		]
+		
+		QueryActivityDsl.runQuery(mmPath, modelPath, queryDef)
+		
+		assertEquals(false, fails)
+	}
+	
+	
+	@Test
 	def void validate_attribute_unique_name() {
 		def mmPath = './model/CD.emf'
 		def modelPath = './model/cd_model.xmi'
@@ -35,7 +57,7 @@ class QueryActivityDsl_CD_test {
 		
 		def queryDef = [
 			context: 'Class',
-			where: { it.attr.size() > 1 },
+			where: { it.attr.size() > 0 },
 			query: {
 				def idCounts = it.attr.countBy { it.name }
 				def repeatedIds = idCounts.findAll { k, v -> v > 1 }.keySet()
